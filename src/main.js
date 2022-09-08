91ec6c409555bc96c1871dec8aee34c674c84456
Felipe Damasceno <felipesdam@outlook.com>
2022-09-08 14:47:52 -0300

    First screen

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-createApp(App).use(router).mount('#app')
+import 'animate.css';
+import '@a/css/tailwind.css';
+import '@a/css/fonts.css';
+
+createApp(App).use(router).mount('#app');
